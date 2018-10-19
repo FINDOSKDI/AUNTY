e8c0fec3424717e9a49202de96394576e36b7684
FINDOSKDI <FINDOSKDI@FINDOSKDI.noreply.github.com>
2018-10-19 20:00:13 +0200

    .

---
+++ render.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import sys
+import pygraphviz as pgv
+str=""" graph {
+A -- B
+}"""
+G = pgv.AGraph(str)
+G.layout(prog="dot")
+G.draw("tmp/aa.png")
---
+++ renderGraph.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys
+import pygraphviz as pgv
+
+G = pgv.AGraph(sys.argv[1])
+G.layout(prog="dot")
+G.draw(sys.argv[2])
