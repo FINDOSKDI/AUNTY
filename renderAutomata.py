fcdc2a172123c341481b1e31028716461a5a35e6
FINDOSKDI <FINDOSKDI@gmail.com>
2018-11-26 14:36:07 +0100

    .

--- renderAutomata.py
+++ renderAutomata.py
@@ -2,8 +2,13 @@
 import sys
 import pygraphviz as pgv
 from metamodelo import *
+
 with open(sys.argv[1], 'r') as f:
     au = str(f.read())
-G = pgv.AGraph(automata2dot(mm.model_from_str(au)))
-#G.layout(prog="dot")
-G.draw(sys.argv[2], prog="dot")
+
+stri = automata2dot(mm.model_from_str(au))
+with open(sys.argv[2]+'.source', 'w') as f:
+    f.write(stri)
+
+G = pgv.AGraph(stri)
+G.draw(sys.argv[2], prog='dot')
