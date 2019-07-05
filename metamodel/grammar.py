fcd3cb37d20b6273719b35285d287c954ac755d4
FINDOSKDI <FINDOSKDI@gmail.com>
2019-07-05 23:56:55 +0200

    .

--- metamodel/grammar.py
+++ metamodel/grammar.py
@@ -26,8 +26,6 @@ VarTail   : ',' head=V;
 
 Action    : '?' ID | '!' ID;
 """
-
-
 tracegrammar = """
 Trace : '(' act=Action const=Constlist  ')' tail*=TraceTail;
 TraceTail : ',' '(' act=Action const=Constlist  ')';
@@ -48,36 +46,53 @@ Action: Input | Output;
 Input: '?' name=ID vars*=ID[',' eolterm];
 Output: '!' name=ID exprs*=Expression[',' eolterm];
 Expression: Expression1;
-Expression1:       l=Expression2 (type='+' r=Expression1)*;
-Expression2:       l=Expression3 (type='-' r=Expression2)*;
-Expression3:       l=Expression4 (type='*' r=Expression3)*;
-Expression4:       l=Expression5 (type='/' r=Expression4)*;
-Expression5:      type=Term | '(' in=Expression1 ')';
-Term: type=Var | type=Val;
+Expression1:       l=Expression2 (ctype='+' r=Expression1)*;
+Expression2:       l=Expression3 (ctype='-' r=Expression2)*;
+Expression3:       l=Expression4 (ctype='*' r=Expression3)*;
+Expression4:       l=Expression5 (ctype='/' r=Expression4)*;
+Expression5:      ctype=Term | '(' ins=Expression1 ')';
+Term: ctype=Var | ctype=Val;
 Var: var=ID;
 Val: val=FLOAT;
 True: str=/TRUE|True|true/;
 Constraints:  /\s*/ constrs+=Constraint[',' eolterm] |   constrs=True;
-Constraint: type=BinRel | type=TerRel;
-BinRel: l=Expression type='<=' r=Expression '['delta=FLOAT']' |
-        l=Expression type='='  r=Expression '['delta=FLOAT']' |
-        l=Expression type='>=' r=Expression '['delta=FLOAT']';
+Constraint: ctype=BinRel | ctype=TerRel;
+BinRel: l=Expression ctype='<=' r=Expression '['delta=FLOAT']' |
+        l=Expression ctype='='  r=Expression '['delta=FLOAT']' |
+        l=Expression ctype='>=' r=Expression '['delta=FLOAT']';
 TerRel: first=Expression '<=' second=Expression '<=' third=Expression '['delta=FLOAT']';
 VTransform:  var=ID '=' expr=Expression ;
-VTransforms:     /\s*/ vtransfms+=VTransform[',' eolterm] | type='Identity';
+VTransforms:     /\s*/ vtransfms+=VTransform[',' eolterm] | ctype='Identity';
 """
 
+class Expression1:
+    def __init__(self, parent, l, ctype, r):
+        pass
+class Expression2:
+    def __init__(self, parent, l, ctype, r):
+        pass
+class Expression3:
+    def __init__(self, parent, l, ctype, r):
+        pass
+class Expression4:
+    def __init__(self, parent, l, ctype, r):
+        pass
+class Expression5:
+    def __init__(self, parent, ins, ctype):
+        pass
 
-expregrammar = """Expression1:       l=Expression2 (type='+' r=Expression1)*;
-Expression2:       l=Expression3 (type='-' r=Expression2)* ;
-Expression3:       l=Expression4 (type='*' r=Expression3)* ;
-Expression4:       l=Expression5 (type='/' r=Expression4)* ;
-Expression5:      type=Term | '(' in=Expression1 ')';
-Term: type=Var | type=Val;
-Var: var=ID;
-Val: val=FLOAT;"""
+class VTransforms:
+    def __init__(self, parent, vtransfms, ctype):
+        self.parent = parent
+        self.ctype = ctype
+        self.vtransfms = vtransfms
 
-d = """GODEL
+listclasses = [VTransforms]
+
+def test_1():
+    from textx import metamodel_from_str
+    mm = metamodel_from_str(altgrammar, classes=listclasses)
+    d = """GODEL
 X,Y,Z
 s0,s1,s2,s3
 s0
@@ -100,25 +115,4 @@ s0,s3
 GODEL
 Y=X+1 [0.2], Z<=5<=X [0.7]
 X=Y*Z/3, Y=0"""
-
-
-testgrammar = """Constraints: type='TRUE' | constrs*=Constraint[',' eolterm];
-Expression: Expression1;
-Expression1:       l=Expression2 (type='+' r=Expression1)*;
-Expression2:       l=Expression3 (type='-' r=Expression2)*;
-Expression3:       l=Expression4 (type='*' r=Expression3)*;
-Expression4:       l=Expression5 (type='/' r=Expression4)*;
-Expression5:      type=Term | '(' in=Expression1 ')';
-Term: type=Var | type=Val;
-Var: var=ID;
-Val: val=FLOAT;
-
-Constraint: type=BinRel | type=TerRel;
-BinRel: l=Expression type='<=' r=Expression '['delta=FLOAT']';
-
-TerRel: first=Expression '<=' second=Expression '<=' third=Expression '['delta=FLOAT']';"""
-
-def test_1():
-    from textx import metamodel_from_str
-    mm = metamodel_from_str(altgrammar)
     mm.model_from_str(d)
