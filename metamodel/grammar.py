020c0038562f26fa6dfc334396f39a2cd750f612
FINDOSKDI <FINDOSKDI@gmail.com>
2019-07-05 23:15:13 +0200

    .

--- metamodel/grammar.py
+++ metamodel/grammar.py
@@ -36,3 +36,89 @@ Constlist : '(' head=R tail*=ConstTail ')' | '()';
 ConstTail : ',' head=R;
 R    : value=FLOAT;
 """
+altgrammar = """
+Automata: tnorm=Tnorm vars=Vars states=States inistate=ID transitions*=Transition;
+Tnorm: type=Godel | type=Hamacher;
+Godel: str=/GODEL|Godel|godel/ ;
+Hamacher: str=/HAMACHER|Hamacher|hamacher/ ;
+Vars: /\s*/ list*=ID[',' eolterm];
+States: /\s*/ list*=ID[',' eolterm];
+Transition: from=ID ',' to=ID act=Action tnorm=Tnorm constrs=Constraints vt=VTransforms;
+Action: Input | Output;
+Input: '?' name=ID vars*=ID[',' eolterm];
+Output: '!' name=ID exprs*=Expression[',' eolterm];
+Expression: Expression1;
+Expression1:       l=Expression2 (type='+' r=Expression1)*;
+Expression2:       l=Expression3 (type='-' r=Expression2)*;
+Expression3:       l=Expression4 (type='*' r=Expression3)*;
+Expression4:       l=Expression5 (type='/' r=Expression4)*;
+Expression5:      type=Term | '(' in=Expression1 ')';
+Term: type=Var | type=Val;
+Var: var=ID;
+Val: val=FLOAT;
+True: str=/TRUE|True|true/;
+Constraints:  /\s*/ constrs+=Constraint[',' eolterm] |   constrs=True;
+Constraint: type=BinRel | type=TerRel;
+BinRel: l=Expression type='<=' r=Expression '['delta=FLOAT']' |
+        l=Expression type='='  r=Expression '['delta=FLOAT']' |
+        l=Expression type='>=' r=Expression '['delta=FLOAT']';
+TerRel: first=Expression '<=' second=Expression '<=' third=Expression '['delta=FLOAT']';
+VTransform:  var=ID '=' expr=Expression ;
+VTransforms:     /\s*/ vtransfms+=VTransform[',' eolterm] | type='Identity';
+"""
+
+
+expregrammar = """Expression1:       l=Expression2 (type='+' r=Expression1)*;
+Expression2:       l=Expression3 (type='-' r=Expression2)* ;
+Expression3:       l=Expression4 (type='*' r=Expression3)* ;
+Expression4:       l=Expression5 (type='/' r=Expression4)* ;
+Expression5:      type=Term | '(' in=Expression1 ')';
+Term: type=Var | type=Val;
+Var: var=ID;
+Val: val=FLOAT;"""
+
+d = """GODEL
+X,Y,Z
+s0,s1,s2,s3
+s0
+s0,s1
+!O1 x*9
+HAMACHER
+X<=4 [0.62]
+Identity
+s0,s2
+?Input2 X,Y,Z
+GODEL true
+X=Y*Z/3, Y=0
+s0,s3
+!O1 x*9
+HAMACHER
+X<=4 [0.62]
+Identity
+s0,s3
+?Input2 X,Y,Z
+GODEL
+Y=X+1 [0.2], Z<=5<=X [0.7]
+X=Y*Z/3, Y=0"""
+
+
+testgrammar = """Constraints: type='TRUE' | constrs*=Constraint[',' eolterm];
+Expression: Expression1;
+Expression1:       l=Expression2 (type='+' r=Expression1)*;
+Expression2:       l=Expression3 (type='-' r=Expression2)*;
+Expression3:       l=Expression4 (type='*' r=Expression3)*;
+Expression4:       l=Expression5 (type='/' r=Expression4)*;
+Expression5:      type=Term | '(' in=Expression1 ')';
+Term: type=Var | type=Val;
+Var: var=ID;
+Val: val=FLOAT;
+
+Constraint: type=BinRel | type=TerRel;
+BinRel: l=Expression type='<=' r=Expression '['delta=FLOAT']';
+
+TerRel: first=Expression '<=' second=Expression '<=' third=Expression '['delta=FLOAT']';"""
+
+def test_1():
+    from textx import metamodel_from_str
+    mm = metamodel_from_str(altgrammar)
+    mm.model_from_str(d)
